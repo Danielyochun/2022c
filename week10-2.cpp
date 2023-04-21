517a481e98241840d30a716c879b71c804210d74
Danielyochun <114201486+Danielyochun@users.noreply.github.com>
2023-04-21 11:41:27 +0800

    Add files via upload

---
+++ week10-2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int singleNumber(int* nums, int numsSize){
+    int ans=0;
+    for(int i=0;i<numsSize;i++){
+        ans=ans^nums[i];
+    }
+    return ans;
+}
+}
---
+++ week10-3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int hammingWeight(uint32_t n) {
+    int ans=0;
+    while(n>0){
+        int now=n%2;
+        if(now==1)ans++;
+        n=n/2;
+    }
+    return ans;
+}
+}
